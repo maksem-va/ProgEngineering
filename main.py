8fae33e1d2998a9919d250ba29e3ddf12179f837
Vasiliy Maksem <maksem.va@outlook.com>
2021-11-30 21:13:31 +0300

    Simple converter from USD to RUB. Cource is currently constant. Closes #1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def cb_page_getter():
-    print("Hello, World!")
+    inp = input('Input request. ').split()
+    print(inp[0] + " " + inp[1].upper() + " equals " + str(float(inp[0]) * 30) + " RUB")
 
+
+# 30 рублей как в 2007
 
 if __name__ == '__main__':
     cb_page_getter()
